fc21ebe1f50b9b2c818549726492f078827a934c
roweeena <roe0320@gmail.com>
2022-02-21 15:54:43 +1100

    registering user / authenticating unique userse

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,20 +1,54 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {createJWT} = require ('../utils/auth')
 const User = mongoose.model('User');
 
 
 exports.listAllUsers = (req, res) => {
   User.find({}, (err, users) => {
     if(err) res.send(err);
-    console.log(res.json(users));
+    res.json(users);
   })
 };
 exports.createUser = (req, res) => {
-  const newUser = new User(req.body)
-  newUser.save((err, user) => {
-    if (err) res.send(err);
-    res.json(user)
-  });
-};
+
+  let {username, password, email} = req.body
+   User.findOne({username: username, email: email})
+        .then(user =>{
+          if(user){
+            return res.status(422).json({errors: [{user: "Email or username is already taken"}]})
+          }else {
+            const newUser = new User(req.body);
+
+            bcrypt.genSalt(10, function(err, salt){
+              bcrypt.hash(password, salt, function(err, hash){
+                if (err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                .then(response => {
+                  res.json({
+                    success:true,
+                    result: response
+                  })
+                }) //then
+                .catch(err => {
+                  res.json({
+                    errors: [{error: err}]
+                  })
+                })//catch
+
+              })//bcrypt.hash
+            })//bcrupt.genSalt
+          }//else
+   }) //then
+};//createUser
+
+// newUser.save((err, user) => {
+//   if (err) res.send(err);
+//   res.json(user)
+// });
+
 
 exports.loginUser = (req,res) => {
   User.findOne(
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const  cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const path = require("path");
+
 
 dotenv.config()
 global.Book = require('./api/models/bookModel');
